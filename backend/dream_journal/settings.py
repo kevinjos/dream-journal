5cc9dd2103aab1628f995431a85450b28a69cefc
kevinjos <kjs@protonmail.com>
2025-08-24 11:04:24 -0700

    CSRF cleanup

--- backend/dream_journal/settings.py
+++ backend/dream_journal/settings.py
@@ -72,7 +72,6 @@ MIDDLEWARE = [
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "allauth.account.middleware.AccountMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
