3424f1ddcc27672d675f8df1fa5c67f0c7fc53b7
kevinjos <kjs@protonmail.com>
2025-08-31 15:53:42 -0700

    simplify celery config

--- backend/celery_config.py
+++ backend/celery_config.py
@@ -1,11 +1,6 @@
 import os
 
 from django.conf import settings
-from kombu import Queue
-
-# Queue name constants
-QUEUE_DEFAULT = "default"
-QUEUE_IMAGE_GENERATION = "image_generation"
 
 broker_url = (
     "redis://localhost:9003/0"
@@ -32,16 +27,6 @@ result_serializer = "json"
 accept_content = ["json"]
 timezone = "UTC"
 enable_utc = True
-
-task_routes = {
-    "dreams.tasks.generate_dream_image": {"queue": QUEUE_IMAGE_GENERATION},
-}
-
-task_default_queue = QUEUE_DEFAULT
-task_queues = (
-    Queue(QUEUE_DEFAULT),
-    Queue(QUEUE_IMAGE_GENERATION),
-)
 
 worker_prefetch_multiplier = 1
 task_acks_late = True
